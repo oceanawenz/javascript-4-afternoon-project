8327f505b5b84d3e9324949d6360086d41dce8ac
Oceana Wenz <oceana.wenz@gmail.com>
2019-05-03 15:54:44 -0700

    done

--- classes.js
+++ classes.js
@@ -29,8 +29,17 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
-
+class Employee {
+  constructor(first_name, last_name, email, age) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+    this.email = email;
+    this.age = age;
+    this.makeWidget = function() {
+      return this.first_name + " " + this.last_name + " " + "Widget";
+    };
+  }
+}
 
 ////////// PROBLEM 2 //////////
 
@@ -47,8 +56,18 @@
   Call your new class Manager
 */
 
-//Code Here
-
+class Manager extends Employee {
+  constructor(first_name, last_name, email, age, reports) {
+    super(first_name, last_name, email, age);
+    this.reports = [];
+  }
+  hire(Employee) {
+    this.reports.push(Employee);
+  }
+  fire(Employee) {
+    this.reports.slice(Employee, 1);
+  }
+}
 
 ////////// PROBLEM 3 //////////
 
@@ -71,9 +90,34 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
-
-
+class ProgressiveManager extends Employee {
+  constructor(first_name, last_name, email, age) {
+    super(first_name, last_name, email, age);
+    this.title = "Not a manager";
+    this.bonus = 0;
+    this.reports = [];
+  }
+  hire(Employee) {
+    this.reports.push(Employee);
+    if (this.reports.length === 0) {
+      this.title;
+    } else if (this.reports.length >= 1 && this.reports.length <= 3) {
+      this.title = "Barely Manager";
+    } else if (this.reports.length >= 4 && this.reports.length <= 10) {
+      this.title = "Mostly Manager";
+    } else if (this.reports.length >= 11 && this.reports.length <= 50) {
+      this.title = "Manager";
+    } else if (this.reports.length >= 51 && this.reports.length <= 100) {
+      this.title = "Manager Plus";
+    } else if (this.reports.length >= 100) {
+      this.title = "Bestest Manager";
+    }
+  }
+  fire(Employee) {
+    this.reports.slice(Employee, 1);
+    this.bonus += 100;
+  }
+}
 
 ////////// PROBLEM 4 - Black Diamond //////////
 
@@ -99,5 +143,3 @@
 */
 
 //Code Here
-
-
